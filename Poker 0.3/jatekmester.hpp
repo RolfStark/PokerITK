c8881f737a18c27e12ffaf37aadbec024aaedf4b
RolfStark <munkaberca@gmail.com>
2026-05-05 17:55:33 +0200

    Add files via upload

---
+++ Poker 0.3/jatekmester.hpp
@@ -0,0 +1,34 @@
+#ifndef CLIONGRAPHICSKIT_JATEKMESTER_HPP
+#define CLIONGRAPHICSKIT_JATEKMESTER_HPP
+
+#include "graphics.hpp"
+#include "Grafikai/kepbetolto.hpp"
+
+using namespace std;
+
+struct Kartyak {
+    string id;
+    string suit;
+    int value;
+};
+struct Score {
+    string name;
+    int value;
+};
+class Jatekmester {
+    void fill(string name);
+    void filled();
+    void scores();
+public:
+    Jatekmester();
+    void szabalyok(vector <Kartyak>v);
+    void alap();
+protected:
+    vector<Score> _rank;
+    vector<Kartyak> _tied;
+    vector<Kartyak> _kozos;
+    vector<Kepbetolto*> _w;
+    vector<Kartyak> _k;
+};
+
+#endif //CLIONGRAPHICSKIT_JATEKMESTER_HPP
